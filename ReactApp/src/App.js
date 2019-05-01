dace0538b509510be1d5184b436c8c45d900350b
Katie Lewis <klewi314@gmail.com>
2019-04-30 19:40:53 -0700

    Initial upload

---
+++ ReactApp/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./App.css";
+import Navbar from "./components/Navbar/navbar.js";
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Navbar />
+      </>
+    );
+  }
+}
+
+export default App;
---
+++ ReactApp/src/components/Navbar/navbar.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./navbar.css";
+
+class Navbar extends Component {
+  render() {
+    return <div>My App</div>;
+  }
+}
+
+export default Navbar;
